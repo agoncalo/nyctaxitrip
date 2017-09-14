022e433f78d3766c638ac12b1331a934db05ee68
christianrfg <christianrfg@gmail.com>
2017-09-13 22:38:27 -0300

    decision tree regression

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 12 21:42:33 2017
+Created on Wed Sep 13 22:15:08 2017
 
 @author: Christian R. F. Gomes
-@title: Support Vector Regression
 """
 
 import numpy as np
@@ -25,39 +24,28 @@ test = pd.read_csv('~/Documentos/Ciencia da Computacao/Machine Learning/New York
 train.dtypes
 # One Hot Encoding
 train = pd.get_dummies(train, columns=['store_and_fwd_flag'], prefix=['frag'])
+test = pd.get_dummies(test, columns=['store_and_fwd_flag'], prefix=['frag'])
 
 # Analise basica sem as colunas id, pickup_datetime e dropoff_datetime
 train.drop(train.columns[[0, 2, 3]], axis=1, inplace=True)
 train = train[['vendor_id', 'passenger_count', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'frag_N', 'frag_Y', 'trip_duration']]
 
-X = train.iloc[0:100, 0:8].values
-y = train.iloc[0:100, 8].values
+y_ids = test['id']      # Para arquivo de submissao
+test.drop(test.columns[[0, 2]], axis=1, inplace=True)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = y.reshape(-1, 1)
-y = sc_y.fit_transform(y)
+X_train = train.iloc[:, 0:8].values
+y_train = train.iloc[:, 8].values
 
-#--------------------------------------------------------------------------
+X_test = test.iloc[:, 0:8].values
 
-# TRAINING AND TEST SET
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 12)
+#-------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------
+# MODELO
 
-# CRIACAO E AVALIACAO DO MODELO
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=12)
+regressor.fit(X_train, y_train)
 
-# Criacao do modelo SVR
-from sklearn.svm import SVR
-svr_regressor = SVR(kernel='rbf')
-y = y.ravel()
-svr_regressor.fit(X, y)
+y_pred = regressor.predict(X_test)
 
-from sklearn import model_selection
-kfold = model_selection.KFold(n_splits=10, random_state=12)
-results = model_selection.cross_val_score(svr_regressor, X, y, cv=kfold, scoring='accuracy')
-
+#-------------------------------------------------------------------------
