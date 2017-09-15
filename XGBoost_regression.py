5cdf27ef8e912dad8939b81512c258ca0ff81936
christianrfg <christianrfg@gmail.com>
2017-09-15 04:05:21 -0300

    last update

--- XGBoost_regression.py
+++ XGBoost_regression.py
@@ -14,9 +14,8 @@ import pandas as pd
 #--------------------------------------------------------------------------
 
 # BASES DE DADOS
-
-train = pd.read_csv('~/Documentos/Ciencia da Computacao/Machine Learning/New York City Taxi Trip Duration - Kaggle Competition/Data/train.csv', header=0)
-test = pd.read_csv('~/Documentos/Ciencia da Computacao/Machine Learning/New York City Taxi Trip Duration - Kaggle Competition/Data/test.csv', header=0)
+train = pd.read_csv('~/Documentos/Ciencia da Computacao/Machine Learning/New York City Taxi Trip Duration - Kaggle Competition/Data/train.csv', header=0, names=['ID', 'VENDOR_ID', 'P_DATETIME', 'D_DATETIME', 'PASSAGENR_COUNT', 'P_LONGITUDE', 'P_LATITUDE', 'D_LONGITUDE', 'D_LATITUDE', 'FLAG', 'T_DURATION'], parse_dates=[2, 3])
+test = pd.read_csv('~/Documentos/Ciencia da Computacao/Machine Learning/New York City Taxi Trip Duration - Kaggle Competition/Data/test.csv', header=0, names=['ID', 'VENDOR_ID', 'P_DATETIME', 'PASSAGENR_COUNT', 'P_LONGITUDE', 'P_LATITUDE', 'D_LONGITUDE', 'D_LATITUDE', 'FLAG'], parse_dates=[2])
 
 #--------------------------------------------------------------------------
 
@@ -24,19 +23,29 @@ test = pd.read_csv('~/Documentos/Ciencia da Computacao/Machine Learning/New York
 
 train.dtypes
 # One Hot Encoding
-train = pd.get_dummies(train, columns=['store_and_fwd_flag'], prefix=['frag'])
-test = pd.get_dummies(test, columns=['store_and_fwd_flag'], prefix=['frag'])
+train = pd.get_dummies(train, columns=['FLAG'])
+test = pd.get_dummies(test, columns=['FLAG'])
 
-# Analise basica sem as colunas id, pickup_datetime e dropoff_datetime
-train.drop(train.columns[[0, 2, 3]], axis=1, inplace=True)
-train = train[['vendor_id', 'passenger_count', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'frag_N', 'frag_Y', 'trip_duration']]
+# DATA
+type(train['P_DATETIME'])
+train['P_DATETIME'] = train['P_DATETIME'].dt.strftime('%j')
+train.drop(train.columns[[0, 3]], axis=1, inplace=True)
+train = train[['VENDOR_ID', 'P_DATETIME', 'PASSAGENR_COUNT', 'P_LONGITUDE', 'P_LATITUDE', 'D_LONGITUDE', 'D_LATITUDE', 'FLAG', 'T_DURATION']]
 
-y_ids = test['id']      # Para arquivo de submissao
-test.drop(test.columns[[0, 2]], axis=1, inplace=True)
+test['P_DATETIME'] = test['P_DATETIME'].dt.strftime('%j')
+y_ids = test['ID']      # Para arquivo de submissao
+test.drop(test.columns[[0]], axis=1, inplace=True)
 
-X_train = train.iloc[:, 0:8].values
-y_train = train.iloc[:, 8].values
+#==============================================================================
+# # Analise basica sem as colunas id, pickup_datetime e dropoff_datetime
+# train.drop(train.columns[[0, 2, 3]], axis=1, inplace=True)
+# train = train[['vendor_id', 'passenger_count', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'frag_N', 'frag_Y', 'trip_duration']]
+# y_ids = test['id']      # Para arquivo de submissao
+# test.drop(test.columns[[0, 2]], axis=1, inplace=True)
+#==============================================================================
 
+X_train = train.iloc[:, 0:9].values
+y_train = train.iloc[:, 9].values
 X_test = test.iloc[:, 0:8].values
 
 #-------------------------------------------------------------------------
